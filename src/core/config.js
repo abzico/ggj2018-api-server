8486956e0e0d1919c7aebed671865a4680937f06
Wasin Thonkaew <haxpor@gmail.com>
2018-01-26 10:10:27 +0800

    Set arbitrary listening port

--- src/core/config.js
+++ src/core/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	boundRadius: 100000,	// in metres
 	baseURL: "/<your path here>",
-	serverListenPort: 5020,
+	serverListenPort: 9999,
 	tokenTTL: 259200,		// expire time for token in seconds
 	dbPath: "/<your db path here>"
 }
